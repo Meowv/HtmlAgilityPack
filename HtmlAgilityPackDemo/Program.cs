a3cfc7527b2fb8ca33085feece9f308084efc364
unknown <qix@gas.cn>
2017-09-07 09:40:04 +0800

    HtmlAgilityPack

---
+++ HtmlAgilityPackDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace HtmlAgilityPackDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var url = "https://meiriyiwen.com/random/";
+
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument doc = web.Load(url);
+                HtmlNode node = doc.DocumentNode.SelectSingleNode("//*[@id=\"article_show\"]");
+                node.Descendants()
+                    .Where(n => n.Name == "script" || n.Name == "style" || n.Name == "#comment" || n.Id == "bdshare")
+                    .ToList().ForEach(n => n.Remove());
+                var title = node.SelectSingleNode("//h1").InnerText;
+                var author = node.SelectSingleNode("//*[@class=\"article_author\"]").InnerText;
+                var article = node.SelectSingleNode("//*[@class=\"article_text\"]").InnerText.TrimStart();
+
+                Console.WriteLine(title + "\r\n");
+                Console.WriteLine(author + "\r\n");
+                Console.WriteLine(article);
+
+                StreamWriter stream = new StreamWriter(@"F:\Article\" + title + "-" + author + ".txt");
+
+                stream.WriteLine(title + "\r\n");
+                stream.WriteLine(author + "\r\n");
+                stream.WriteLine(article);
+
+                stream.Close();
+                stream.Dispose();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# HtmlAgilityPack
-HtmlAgilityPack爬取每日一文文章，使用学习...
+# HtmlAgilityPack？
+
+HtmlAgilityPack 是 .NET 下的一个 HTML 解析类库。支持用 XPath 来解析 HTML 。命名空间： HtmlAgilityPack
+
+# 核心程序代码
+
+```
+    var url = "https://meiriyiwen.com/random/";
+
+    HtmlWeb web = new HtmlWeb();
+    HtmlDocument doc = web.Load(url);
+    HtmlNode node = doc.DocumentNode.SelectSingleNode("//*[@id=\"article_show\"]");
+    node.Descendants()
+        .Where(n => n.Name == "script" || n.Name == "style" || n.Name == "#comment" || n.Id == "bdshare")
+        .ToList().ForEach(n => n.Remove());
+        
+    var title = node.SelectSingleNode("//h1").InnerText;
+    var author = node.SelectSingleNode("//*[@class=\"article_author\"]").InnerText;
+    var article = node.SelectSingleNode("//*[@class=\"article_text\"]").InnerText.TrimStart();
+```
